4f247c631245d55046c8e4d9b0475b4237e0d438
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-17 09:24:25 +0000

    Install and configure ESLint and Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-    entry: './src/index.js',
-    devtool: 'inline-source-map',
-    devServer: {
-        static: './dist',
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-        clean: true,
-    },
-    module: {
-        rules: [{ 
-                test: /\.txt$/, 
-                use: 'raw-loader'
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader', 'css-loader'],
-            },
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                    options: {
-                    presets: ['@babel/preset-env']
-                    }
-                }
-            },
-        ],
-    }
+  entry: "./src/index.js",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.txt$/,
+        use: "raw-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: "html-loader" }],
+      },
+    ],
+  },
 };
