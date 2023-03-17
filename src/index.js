dc6cfe73ff2c9ad056d202c1fe620af08cfadbbd
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-17 11:35:09 +0000

    Convert function to an ES6 module

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 import "./style.css";
+const showHideDropdown = (() => {
+  const menu = document.querySelector(".menu");
+  const menuItem = document.querySelectorAll(".hidden");
+  menu.addEventListener("mouseover", function () {
+    menuItem.forEach((item) => {
+      item.style.display = "block";
+    });
+  });
+  menu.addEventListener("mouseout", function () {
+    menuItem.forEach((item) => {
+      item.style.display = "none";
+    });
+  });
+})();
 
-const menu = document.querySelector(".menu");
-const menuItem = document.querySelectorAll(".hidden");
-menu.addEventListener("mouseover", function () {
-  menuItem.forEach((item) => {
-    item.style.display = "block";
-  });
-});
-menu.addEventListener("mouseout", function () {
-  menuItem.forEach((item) => {
-    item.style.display = "none";
-  });
-});
+export { showHideDropdown };
