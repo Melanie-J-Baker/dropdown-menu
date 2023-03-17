aaa924c2688b947c011cc3f5d77a70ac1ead3423
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-17 11:27:45 +0000

    Build dropdown menu that expands on mouseover

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-import './style.css'
+import "./style.css";
+
+const menu = document.querySelector(".menu");
+const menuItem = document.querySelectorAll(".hidden");
+menu.addEventListener("mouseover", function () {
+  menuItem.forEach((item) => {
+    item.style.display = "block";
+  });
+});
+menu.addEventListener("mouseout", function () {
+  menuItem.forEach((item) => {
+    item.style.display = "none";
+  });
+});
