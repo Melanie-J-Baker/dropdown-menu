e2badc72f9d7f0af46301d17b07568b5357155f8
Melanie-J-Baker <baker_mel@hotmail.com>
2023-03-17 09:04:58 +0000

    Set up webpack and add loaders, npm scripts, devtool and devserver

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+module.exports = {
+    entry: './src/index.js',
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    output: {
+        filename: 'main.js',
+        path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    module: {
+        rules: [{ 
+                test: /\.txt$/, 
+                use: 'raw-loader'
+            },
+            {
+                test: /\.css$/i,
+                use: ['style-loader', 'css-loader'],
+            },
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                    presets: ['@babel/preset-env']
+                    }
+                }
+            },
+        ],
+    }
+};
